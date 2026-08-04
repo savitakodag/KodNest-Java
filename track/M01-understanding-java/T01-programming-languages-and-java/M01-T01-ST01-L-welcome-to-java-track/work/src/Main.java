da629ccb21fb26162925c6e5b43f5d48f95fb92f
savita <savitakodag9113@gmail.com>
2026-08-04 22:13:41 +0530

    added msg

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Display the student's learning track.
+        System.out.println("Track: Java Development");
+
+        // Display the student's daily commitment.
+        System.out.println("Daily Practice: 8 Hours");
+    }
+}
--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,6 +1,9 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
-       System.out.println("Welcome to kodNest");
+        //Write your code here
+        System.out.println("Welcome to kodNest");
+        System.out.println("Welcome to java");
     }
 }
