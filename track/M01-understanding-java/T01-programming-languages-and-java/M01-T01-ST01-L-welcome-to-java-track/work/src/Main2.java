886416447ce0fcee8c8a127d9e2673be5f2694bf
savita <savitakodag9113@gmail.com>
2026-08-05 08:00:02 +0530

    Placement

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Main2.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("---MY PLACEMENT GOAL ---");
+        System.out.println("Role: Software Developer");
+        System.out.println("Target: Get Placed");
+        System.out.println("Daily Practice: 8 Hours");
+    }
+}
