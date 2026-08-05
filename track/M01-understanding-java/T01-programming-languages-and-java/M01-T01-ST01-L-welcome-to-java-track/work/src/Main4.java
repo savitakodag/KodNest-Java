31a6a5f0a195ed39ff19a280c93ef38f0ffac8bb
savita <savitakodag9113@gmail.com>
2026-08-05 08:07:41 +0530

    Student ID details

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Main4.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // This program prints a student ID card.
+        System.out.println("----------------------");
+        System.out.println("STUDENT PROFILE");
+        System.out.println("Name: Alex");
+        System.out.println("Course: Java");
+        System.out.println("Level: Beginner");
+        System.out.println("----------------------");
+    }
+}
