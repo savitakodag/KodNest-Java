736983a5bba909706c43150d57362237c4aaf1f8
savita <savitakodag9113@gmail.com>
2026-08-07 13:06:57 +0530

    if else to switch

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/M02/Program7.java
@@ -0,0 +1,28 @@
+
+public class Program7 {
+
+    public static void main(String[] args) {
+
+        int day = 6;
+
+        // Write the switch statement
+        switch (day) {
+
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Learning Day");
+                break;
+
+            case 6:
+            case 7:
+                System.out.println("Revision Day");
+                break;
+
+            default:
+                System.out.println("Invalid Day");
+        }
+    }
+}
